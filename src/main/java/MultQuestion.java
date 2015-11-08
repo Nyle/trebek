830b194f6df95b1386ea4fe32473a0e931a42c83
Crazy AZ <Crazy AZ>
2015-11-07 20:17:35 -0500

    Fucking committing

---
+++ src/main/java/MultQuestion.java
@@ -0,0 +1,21 @@
+/**
+ * Assigns a simple multiplication question
+ *
+ * @author Aarav Singh
+ * @version (2015.11.07)
+ */
+
+public class WordsQuestion extends Question {
+    //~ Fields ............................................
+
+
+    //~ Constructor .......................................
+    public WordsQuestion(){
+
+    }
+
+    //~ Methods ...........................................
+    public void getAnswer(){
+        answer =
+    }
+}
